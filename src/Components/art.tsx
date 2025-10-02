aee170f325669901edc2d198c315b9bf2f9820d5
Vladislav-Baidyk <vladyk.baidyk@gmail.com>
2025-10-02 22:22:35 +0300

    let's try

--- src/Components/art.tsx
+++ src/Components/art.tsx
@@ -6,45 +6,37 @@ function Art() {
       <div id="art" className={styles.artContainer}>
         <div className={styles.artText}>
           <h1>Art 2025</h1>
-          <p>text</p>
         </div>
 
         <div className={styles.containerPictures}>
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_04.jpg"
-            alt="art_04"
-          />
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_05.jpg"
-            alt="art_05"
-          />
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_03.jpg"
-            alt="art_03"
-          />
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_01.jpg"
-            alt="art_01"
-          />
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_06.jpg"
-            alt="art_06"
-          />
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_07.jpg"
-            alt="art_07"
-          />
-          <img
-            style={{ flexGrow: 1 }}
-            src="./public/art/art_02.jpg"
-            alt="art_02"
-          />
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_04.jpg" alt="art_04" />
+            <p>"MAN" B1 PENCIL</p>
+          </div>
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_05.jpg" alt="art_04" />
+            <p>"GUITAR" B1 PENCIL</p>
+          </div>
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_03.jpg" alt="art_04" />
+            <p>"STILL LIFE" B2 ACRYLIC</p>
+          </div>
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_01.jpg" alt="art_04" />
+            <p>"GIRL" B1 PENCIL"</p>
+          </div>
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_06.jpg" alt="art_04" />
+            <p>"MY CULTURE" B1 ACRYLIC </p>
+          </div>
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_07.jpg" alt="art_04" />
+            <p>"MY FIRST MEMORY" B1 ACRYLIC"</p>
+          </div>
+          <div className={styles.picture} style={{ flexGrow: 1 }}>
+            <img src="./art/art_02.jpg" alt="art_04" />
+            <p>"WITCH" B1 ACRYLIC"</p>
+          </div>
         </div>
       </div>
     </Fragment>
